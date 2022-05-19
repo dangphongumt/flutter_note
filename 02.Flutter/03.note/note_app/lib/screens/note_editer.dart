802e516d2e4dbb1bc2a3191cfa48b5616a1b05fb
PhongD <dangphongumt@gmail.com>
2022-05-19 22:09:43 +0700

    Push data to firebase in note_editer

--- 02.Flutter/03.note/note_app/lib/screens/note_editer.dart
+++ 02.Flutter/03.note/note_app/lib/screens/note_editer.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:note_app/styles/app_style.dart';
 
 class NoteEditerScreen extends StatefulWidget {
   const NoteEditerScreen({Key? key}) : super(key: key);
@@ -8,8 +12,74 @@ class NoteEditerScreen extends StatefulWidget {
 }
 
 class _NoteEditerScreenState extends State<NoteEditerScreen> {
+  int color_id = Random().nextInt(AppStyle.cardsColor.length);
+  String date = DateTime.now().toString();
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _mainController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppStyle.cardsColor[color_id],
+      appBar: AppBar(
+        backgroundColor: AppStyle.cardsColor[color_id],
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Add a new note",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Note title',
+              ),
+              style: AppStyle.mainTitle,
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              date,
+              style: AppStyle.dateTitle,
+            ),
+            SizedBox(
+              height: 28.0,
+            ),
+            TextField(
+              controller: _mainController,
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: InputDecoration(
+                  border: InputBorder.none, hintText: 'Note content'),
+              style: AppStyle.mainContent,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppStyle.accentColor,
+        onPressed: () {
+          print("LOG: Saving to firebase");
+          FirebaseFirestore.instance.collection("notes").add({
+            "note_title": _titleController.text,
+            "creation_date": date,
+            "note_content": _mainController.text,
+            "color_id": color_id
+          }).then((value) {
+            print("value.id: ${value.id}");
+            Navigator.pop(context); //return home screen
+          }).catchError(
+              (error) => print("Failed to add new Note due to $error"));
+        },
+        child: Icon(Icons.save),
+      ),
+    );
   }
 }
